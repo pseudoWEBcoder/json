cfeab42ad62b8b9c3e607fc30892c54f75e625e6
alal <betu@maiola.net>
2018-10-21 22:25:45 +0300

    подключил  RedBeanPHP  но не  дописал кнопку  которая  сохраняет  точнее  оно  сохраняется но не  отображается зачеркнутым

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-error_reporting(E_ERROR);
 
+use RedBeanPHP\R;
+
+error_reporting(E_ERROR);
+require 'vendor/autoload.php';
+R::setup('sqlite:' . realpath($df = dirname(__FILE__) . '/db/db.db'));
+$goods = R::dispense('goods');
 $dir = '/sdcard/Download/';
 $dir = 'C:\Users\nik-msk-win10\Desktop\json';
-$dirname = dirname($dir);
+$dirname = realpath($dir);
 if (isset($_REQUEST['file'])) {
     $file = $_REQUEST['file'];
     $format = $_REQUEST['format'];
@@ -18,17 +23,25 @@ $all = scandir($dirname);
 //echo '<hr>';
 foreach ($all as $k => $v) {
     if (file_exists($r = realpath($dirname . '/' . $v)) && ($inf = pathinfo($v)) && isset($inf['extension']) && $inf['extension'] == 'json')
-        $filtered[filemtime($r)] = $inf['filename'];
+        $filtered[] = ['name' => $inf['filename'], 'time' => filemtime($r), 'size' => format_filesize(filesize($r))];
 }
-krsort($filtered, SORT_NUMERIC);
+uksort($arr, function ($a, $b) use ($filtered) {
+
+    return $filtered[$b]['time'] - $filtered[$a]['time'];
+});
 //dump([basename($file,'.json'),$filtered]);
 
 foreach ($filtered as $k => $v) {
-    $checked = ($file == $v) ? ' selected="selected"' : '';
-    $options[] = "<option value=\"$v\"$checked >" . date('d.m.Y h:i:s', $k) . " - $v</option>";
+    $checked = ($file == $v['name']) ? ' selected="selected"' : '';
+    $options[] = "<option value=\"{$v['name']}\"$checked >" . date('d.m.Y h:i:s', $v['time']) . " - {$v['name']} - {$v['size']}</option>";
 }
 //dump($options);
-
+function format_filesize($B, $D = 2)
+{
+    $S = 'kMGTPEZY';
+    $F = floor((strlen($B) - 1) / 3);
+    return sprintf("%.{$D}f", $B / pow(1024, $F)) . ' ' . @$S[$F - 1] . 'B';
+}
 function dump($arr)
 {
     echo '<pre>';
@@ -49,6 +62,21 @@ function execute($path)
     return json_encode($arr, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 }
 
+function save($arr)
+{
+    global $goods;
+    foreach ($arr as $index => $item) {
+        if ($item["document"]["receipt"]["dateTime"] == $_REQUEST['date'])
+            break;
+    }
+    foreach ($item["document"]["receipt"] as $field => $value) {
+        if ($field !== 'items')
+            $goods->$field = $value;
+    }
+    R::store($goods);
+
+
+}
 function execute2($path)
 {
     $c = file_get_contents($path);
@@ -61,6 +89,8 @@ function execute2($path)
         $date2 = DateTime::createFromFormat($f, $v2);
         return $date1 && $date2 ? (int)($date2->format('U') - $date1->format('U')) : 0;
     });
+    if (isset($_REQUEST['action']))
+        save($arr);
     array_walk_recursive($arr, function (&$v, $k) {
         if (is_numeric($v) && in_array($k, ['price', 'totalSum', 'sum', 'cashTotalSum', 'ecashTotalSum']))
             $v = (float)$v / 100;
@@ -69,18 +99,30 @@ function execute2($path)
     );
     $ul = '';
     $icon = [
-        'before' => ['name' => '', 'price' => ' ', 'quantity' => '&times;&nbsp;', 'sum' => '=', 'retailPlaceAddress'=>'&#128506;'],
-        'after' => ['name' => '', 'price' => '&#x20bd; ', 'quantity' => '', 'sum' => '&#x20bd',  'totalSum'=>'&#x20bd; ']];
+        'before' => ['dateTime' => '&#128357;', 'name' => '', 'price' => ' ', 'quantity' => '&times;&nbsp;', 'sum' => '=', 'retailPlaceAddress' => '&#128506; '],
+        'after' => ['name' => '', 'price' => '&#x20bd; ', 'quantity' => '', 'sum' => '&#x20bd', 'totalSum' => '&#x20bd; '],
+        'actions' => ['commit' => '&#10004;']];
     foreach ($arr as $k => $v) {
         $r = $v['document']['receipt'];
         $ul .= '<li>';
-        foreach (['dateTime', 'totalSum', 'user',   'retailPlaceAddress'] as $i)
-            $ul .= ' <span class="' . $i . '">'. '<span class="icon">'.get($i, $icon['before']).'</span>' . ($i == 'dateTime' ? dateFormat($r['dateTime']) : get($i, $r)) .'<span class="icon">'. get($j, $icon['after']).'</span>' .  '</span>';
+        foreach (['dateTime', 'totalSum', 'user', 'retailPlaceAddress'] as $i) {
+            $val = ($i == 'dateTime' ? dateFormat($r['dateTime']) : get($i, $r));
+            if ($val)
+                $ul .= ' <span class="' . $i . '">' . '<span class="icon">' . get($i, $icon['before']) . '</span>' . $val . '<span class="icon">' . get($i, $icon['after']) . '</span>' . '</span>';
+        }
         $ul .= '<ol class="items" start="1">';
         foreach (get('items', $r) as $I => $item) {
             $ul .= '<li>';
             foreach (['name', 'price', 'quantity', 'sum'] as $j)
-                $ul .= "\t" . '<span class="' . $j . '">' . '<span class="icon">'.get($j, $icon['before']).'</span>' . get($j, $item) .'<span class="icon">'. get($j, $icon['after']).'</span>' . '</span>';
+                $ul .= "\t" . '<span class="' . $j . '">' . '<span class="icon">' . get($j, $icon['before']) . '</span>' . get($j, $item) . '<span class="icon">' . get($j, $icon['after']) . '</span>' . '</span>';
+            $ul .= '<ul class="actions">';
+            foreach (['commit'] as $ak => $av) {
+                $ul .= '<li>';
+                $ul .= '<a href="?' . http_build_query(['action' => $av, 'number' => $I, 'date' => $r['dateTime']]) . '">' . get($av, $icon['actions']) . '</a>';
+
+                $ul .= '</li>';
+            }
+            $ul .= '</ul>';///---
             $ul .= '</li>';
         }
         $ul .= '</ol>';
@@ -152,14 +194,23 @@ function get($key, $arr = null, $default = null)
         color: darkgreen;
     }
 
-    .items > li > span.price {
-        color: darkblue;
+    .items > li > span.price, span.sum, span.totalSum {
+        color: darkred;
     }
 
     items > li > span.quantity {
-        color: darkred;
+        color: darkblue;
     }
-    span.icon{color:#555555;}
+
+    span.icon {
+        color: #555555;
+    }
+
+    ul.actions, ul.actions li {
+        display: inline-block;
+        margin: 0;
+        padding: 0;
+    }
 </style>
 <style>
     ul.receipts > li {
@@ -189,12 +240,13 @@ function get($key, $arr = null, $default = null)
     items > li > span.quantity {
         color: darkred;
     }
+
+    span.retailPlaceAddress {
+        font-style: italic;
+        font-weight: normal;
+    }
 </style>
-</
 
-style
-
->
 
 <?php
 echo $format == 'json' ? '			<pre class="hljs json">
